8827ac18e1ca9f9e531b56a0755ef2f3e06fbef6
Theo Gmz <theogamiz31@gmail.com>
2023-07-26 17:37:30 +0200

    profile/photo

--- lib/features/profile/screen/profile_screen.dart
+++ lib/features/profile/screen/profile_screen.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 
 class Profile extends StatefulWidget {
   const Profile({Key? key, this.user}) : super(key: key);
@@ -10,35 +11,22 @@ class Profile extends StatefulWidget {
   State<Profile> createState() => _ProfileState();
 }
 
-
-Future<String?> _getImageUrl(user) async {
-  if (user != null) {
-    try {
-      // Assuming you have a Firebase Storage bucket named "profile_pictures"
-      final ref = FirebaseStorage.instance
-          .ref()
-          .child('profile_pictures/${user!.uid}');
-      return await ref.getDownloadURL();
-    } catch (e) {
-      print('Error fetching image URL: $e');
-      return null;
-    }
-  } else {
-    return null;
-  }
-}
-
 class _ProfileState extends State<Profile> {
+  String? _imageUrl;
+
   @override
   Widget build(BuildContext context) {
-    
     if (widget.user != null) {
       return Column(
         children: [
           SizedBox(height: 100),
           // Afficher l'image dans un cercle
-          
-          
+          _imageUrl != null
+              ? CircleAvatar(
+                  radius: 50,
+                  backgroundImage: NetworkImage("https://firebasestorage.googleapis.com/v0/b/chat-app-4c9df.appspot.com/o/${widget.user!.uid}.png?alt=media&token=${widget.user!.refreshToken}"),
+                )
+              : CircularProgressIndicator(), // You can use any other loading indicator
           Text(widget.user!.email!),
           Text(widget.user!.uid),
         ],
